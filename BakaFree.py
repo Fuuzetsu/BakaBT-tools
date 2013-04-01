9141e8523054be1f3fbc8f1f855d8e857cdb2961
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-04-01 08:43:39 +0100

    new URL page visiting method

--- BakaFree.py
+++ BakaFree.py
@@ -6,10 +6,40 @@ import re
 import shutil
 import socket
 import mechanize
+from HTMLParser import HTMLParser
 
 URL_BASE = 'http://bakabt.me'
-PAGES = 4
-STARTING = 1
+LIMIT = 2
+
+class Maybe(object):
+
+    def __init__(self, val, nothing=False):
+        self.val = val
+        self.nothing = nothing
+
+    def __repr__(self):
+        if self.is_nothing():
+            return 'Nothing'
+        return 'Just %s' % self.val
+
+    def is_nothing(self):
+        return self.nothing
+
+    def get_value(self):
+        if self.nothing:
+            raise Maybe.NothingError
+        return self.val
+
+    class NothingError(Exception):
+        pass
+
+
+Nothing = Maybe(None, nothing=True)
+
+def Just(v):
+    return Maybe(v)
+
+
 
 def getLinks(pageSource):
     sections = []
@@ -36,13 +66,7 @@ def getLinks(pageSource):
     for s in sections:
         extracted.append(re.search(r'<a href="(/\d+[-_\w.]+)" style="color:', s).groups()[0])
 
-    return extracted
-
-def getPages(pageSource):
-    pages = []
-    for i in range(STARTING, PAGES):
-        pages.append('/browse.php?ordertype=size&amp%%3Border=1&amp%%3Blimit=100&amp%%3Bpage=1&order=1&limit=100&page=%d' % i)
-    return pages
+    return [ URL_BASE + x for x in extracted ]
 
 def getTorrents(url):
     while True:
@@ -77,13 +101,33 @@ def download(url):
             if shutil.os.path.getsize(fileName) == fileSize:
                 passed = True
 
+class BakaParser(HTMLParser):
+
+    waiting_for_pages = False
+    page_links = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div':
+            for a in attrs:
+                if a[0] == 'class' and a[1] == 'pager':
+                    BakaParser.waiting_for_pages = True
+
+        if BakaParser.waiting_for_pages and tag == 'a':
+            for a in attrs:
+                if a[0] == 'href':
+                    BakaParser.page_links.append(a[1])
+
+    def handle_endtag(self, tag):
+        if tag == 'div' and BakaParser.waiting_for_pages:
+            BakaParser.waiting_for_pages = False
 
 def search(word):
     request = mechanize.Request('%s/browse.php?q=%s' % (URL_BASE, word))
     response = mechanize.urlopen(request)
     return response.read()
 
-
+def idt(x):
+    return x
 
 def main():
     if len(sys.argv) != 3:
@@ -92,11 +136,64 @@ def main():
 
     logged_in = login(sys.argv[1], sys.argv[2])
 
-    if logged_in:
-        sys.stdout.write('Successfully logged in as %s\n' % sys.argv[1] )
+    if logged_in.is_nothing():
+        sys.stdout.write('Failed to reach the login page.\n')
+    elif logged_in.get_value():
+        sys.stdout.write('Successfully logged in as %s\n' % sys.argv[1])
     else:
         sys.stdout.write('Failed to log in.\n')
         sys.exit(3)
+
+    m_links = mapMaybe(mKlesliComp(getLinks, get_page_source), get_pages())
+
+    if m_links.is_nothing():
+        sys.stderr.write('Got Nothing in the end.\n')
+        sys.exit(3)
+
+    all_links = [ y for x in m_links.get_value() for y in x ]
+
+    sys.stdout.write(all_links.__str__() + '\n')
+    sys.stdout.write(len(all_links).__str__() + '\n')
+
+def maybeBind(m, f):
+    if m.is_nothing():
+        return Nothing
+
+    return f(m.get_value())
+
+def mKlesliComp(f, g):
+    return lambda x: maybeBind(g(x), f)
+
+
+# (a -> b) -> Maybe [a] -> Maybe [b]
+def mapMaybe(f, m):
+    if m.is_nothing():
+        return Nothing
+    return Just([ f(x) for x in m.get_value() ])
+
+def get_page_source(url):
+    try:
+        return Just(mechanize.urlopen(url).read())
+    except mechanize.HTTPError:
+        sys.stderr.write('HTTPError when fetching %s\n' % url)
+        return Nothing
+
+
+def get_pages():
+    try:
+        page_url = ('%s/browse.php?ordertype=size&order=1&limit=100' % URL_BASE)
+        request = mechanize.Request(page_url)
+        response = mechanize.urlopen(request)
+        body = response.read()
+        response.close()
+
+        parser = BakaParser()
+        parser.feed(body)
+        pages = int(BakaParser.page_links[-2].split('=')[-1]) + 1
+        pages = LIMIT if LIMIT < pages else pages
+        return Just([ '%s&page=%d' % (page_url, p) for p in xrange(pages) ])
+    except mechanize.HTTPError:
+        return Nothing
 
 
 def login(username, password):
@@ -104,6 +201,9 @@ def login(username, password):
     response = mechanize.urlopen(request)
     forms = mechanize.ParseResponse(response)
     response.close()
+    if len(forms) < 3:
+        sys.stderr.write('Failed to reach the login page.\n')
+        sys.exit(1)
 
     form = forms[2]
     form['username'] = username
@@ -112,9 +212,10 @@ def login(username, password):
     try:
         login_response = mechanize.urlopen(login_request)
     except mechanize.HTTPError, login_response:
-        sys.stdout.write('HTTPError when logging in...\n')
+        sys.stderr.write('HTTPError when logging in...\n')
+        return Nothing
 
-    return login_response.geturl() == ('%s/index.php' % URL_BASE)
+    return Just(login_response.geturl() == ('%s/index.php' % URL_BASE))
 
 if __name__ == '__main__':
     main()
